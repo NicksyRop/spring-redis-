1c4a5d44bbc4a4bf290f277eaa3b4cb685f0bc7c
Nickson <nnkipkorir@dtbafrica.com>
2024-04-21 12:11:57 +0300

    enable caching , use @Cacheable , @CachePut , @CacheEvict

--- src/main/java/com/nicksy/cache/service/EmployeeService.java
+++ src/main/java/com/nicksy/cache/service/EmployeeService.java
@@ -4,6 +4,9 @@ import com.nicksy.cache.entity.Employee;
 import com.nicksy.cache.exception.ResourceNotFoundException;
 import com.nicksy.cache.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,22 +17,26 @@ public class EmployeeService {
     @Autowired
     EmployeeRepository employeeRepository;
 
+    @Cacheable(value = "employees" , key = "#employeeId", sync = true)
     public Employee getEmployee(UUID employeeId) {
         return employeeRepository.findById(employeeId)
                 .orElseThrow(() ->
                         new ResourceNotFoundException(
                                 "Employee not found:" + employeeId));
     }
-
+    @Cacheable(value = "employees")
     public List<Employee> getAllEmployees() {
         return employeeRepository.findAll();
     }
-
+    @CachePut(value = "employees", key = "#employee.id")
     public Employee saveEmployee(Employee employee) {
         return employeeRepository.save(employee);
     }
 
+    @CacheEvict(value = "employees", allEntries = true)
     public void deleteEmployee(UUID employeeId) {
+        System.out.println(
+                "Removing all employees from the database and cache");
         employeeRepository.deleteById(employeeId);
     }
 }
