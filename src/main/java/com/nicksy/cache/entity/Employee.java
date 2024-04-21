61d21f7df56fb13f6db75bf3acd3615dd6c590ea
Nickson <nnkipkorir@dtbafrica.com>
2024-04-21 11:41:03 +0300

    initial setup

---
+++ src/main/java/com/nicksy/cache/entity/Employee.java
@@ -0,0 +1,25 @@
+package com.nicksy.cache.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String name;
+}
